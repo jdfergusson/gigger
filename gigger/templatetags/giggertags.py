857faf715f1e48bb5f800c0b933e05acfe80d1ce
Joel Fergusson <joel@fergfamily.co.uk>
2024-11-27 10:26:56 +0000

    Now shows unassigned instruments

--- gigger/templatetags/giggertags.py
+++ gigger/templatetags/giggertags.py
@@ -21,6 +21,13 @@ def get_player_assigned_to_instrument(gig_id, instrument_id):
 def get_players_on_gig(gig_id):
     return PlayingAtGig.objects.filter(gig_id=gig_id).order_by('instrument')
 
+@register.simple_tag
+def get_unassigned_instruments(gig_id):
+    gig = Gig.objects.get(id=gig_id)
+    pags = [i.instrument for i in PlayingAtGig.objects.filter(gig_id=gig_id).all()]
+    instruments = [i for i in gig.required_instruments.all() if i not in pags]
+    return instruments
+
 @register.simple_tag(takes_context=True)
 def absolute_url(context, relative_url):
   request = context['request']
